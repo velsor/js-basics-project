fe7825ccca55d0ea76fc72f7fe3fae274e770eb9
velsor <vl.luzan@gmail.com>
2019-09-12 15:38:16 +0300

    добавил слайдер и переключений по стрелкам

--- src/js/carousel.js
+++ src/js/carousel.js
@@ -93,6 +93,133 @@ class Carousel {
     ]
 
     constructor(element) {
+      const layout = `
+      <div id="mainCarousel" class="main-carousel carousel slide">
+        <ol class="carousel-indicators">
+            <li data-target="#mainCarousel" data-slide-to="0" class="carousel-indicator"></li>
+            <li data-target="#mainCarousel" data-slide-to="1" class="carousel-indicator"></li>
+            <li data-target="#mainCarousel" data-slide-to="2" class="carousel-indicator"></li>
+        </ol>
+        <div class="carousel-inner">
+            <!--Вот здесь будет активный слайд-->
+        </div>
+        
+        <button class="carousel-control-prev" href="#mainCarousel" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+        </button>
+        <button class="carousel-control-next" href="#mainCarousel" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </button>
+      </div>
+      `;
+      element.innerHTML = layout;
+      const slideLayout = `
+      <div class="carousel-item active">
+        <img src="assets/images/default-slide-img.jpg" alt="Activelide">
+        <div class="container">
+            <div class="carousel-caption">
+                <h3 class="h1">BEST LAPTOP DEALS</h3>
+                <div>
+                    <a class="btn" href="#" role="button">
+                        View all DEALS
+                        <img src="assets/icons/icon-angle-white.svg" class="ml-3" alt="">
+                    </a>
+                </div>
+            </div>
+        </div>
+      </div>
+      `;
+      const slide = element.querySelector('.carousel-inner');
+      slide.innerHTML = slideLayout;
+
+      const buttonNext = element.querySelector('.carousel-control-next');
+      const buttonPrev = element.querySelector('.carousel-control-prev');
+
+      const slides = [
+        {
+            id: 0,
+            title: 'BEST LAPTOP DEALS'
+        },
+        {
+            id: 1,
+            title: 'BEST HEADPHONES DEALS'
+        },
+        {
+            id: 2,
+            title: 'BEST SPEAKERS DEALS'
+        }
+      ];
+
+      function showNextSlide(event) {
+        let activeSlideTitle = element.querySelector('h3').innerHTML;
+        let nextSlide = '';
+        let slideId = 0;
+        slides.forEach((item) => {
+          let id = item.id;
+          let title = item.title;
+          if (title === activeSlideTitle) {
+            switch (id) {
+              case 0:
+                id += 1;
+                slideId = id;
+                break;
+              case 1:
+                id += 1;
+                slideId = id;
+                break;
+              case 2:
+                id = 0;
+                slideId = id;
+                break;
+            }
+          }
+        });
+        slides.forEach((item) => {
+          let id = item.id;
+          if (slideId === id) {
+            nextSlide = item.title;
+          }
+        });
+        element.querySelector('h3').innerHTML = nextSlide;
+      }
+
+      function showPrevSlide(event) {
+        let activeSlideTitle = element.querySelector('h3').innerHTML;
+        let nextSlide = '';
+        let slideId = 0;
+        slides.forEach((item) => {
+          let id = item.id;
+          let title = item.title;
+          if (title === activeSlideTitle) {
+            switch (id) {
+              case 0:
+                id = 2;
+                slideId = id;
+                break;
+              case 1:
+                id -= 1;
+                slideId = id;
+                break;
+              case 2:
+                id -= 1;
+                slideId = id;
+                break;
+            }
+          }
+        });
+        slides.forEach((item) => {
+          let id = item.id;
+          if (slideId === id) {
+            nextSlide = item.title;
+          }
+        });
+        element.querySelector('h3').innerHTML = nextSlide;
+      }
+
+      buttonNext.addEventListener('click', showNextSlide);
+      buttonPrev.addEventListener('click', showPrevSlide);
     }
 }
 
